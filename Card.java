062c4d1d3565994af1b0cec63f1fc42a2630f915
Geoff Schmit <gcschmit@gmail.com>
2022-02-04 08:24:17 -0600

    refactor Card to be abstract

---
+++ Card.java
@@ -0,0 +1,26 @@
+public abstract class Card
+{
+   private String name;
+
+   public Card()
+   {
+      name = "";
+   }
+
+   public Card(String n)
+   {
+      name = n;
+   }
+
+   public String getName()
+   {
+      return name;
+   }
+
+   public abstract boolean isExpired();
+
+   public String toString()
+   {
+      return "Card holder: " + name;
+   }
+}
