643b2d0bfd0f85127d759f7af0f7ef0c10d58170
Geoff Schmit <gcschmit@gmail.com>
2022-02-10 09:19:50 -0600

    add GUI example #1

---
+++ ClickListener.java
@@ -0,0 +1,16 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Write a description of class ClickListener here.
+ *
+ * @author gcschmit
+ * @version 10feb2022
+ */
+public class ClickListener implements ActionListener
+{
+    public void actionPerformed(ActionEvent event)
+    {
+        System.out.println("button clicked");
+    }
+}
